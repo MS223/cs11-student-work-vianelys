30519be3051beea9052f8f98facb4dca101b3e88
vianelys <vianelys.montero@ms223.org>
2016-03-22 08:16:22 -0400

    Update find_odds.py

--- find_odds.py
+++ find_odds.py
@@ -1,1 +1,6 @@
 list = [3,4,7,13,54,32,653,256,1,41,65,83,92,31]
+def find_odds(input):
+    for whatever in input:
+        if whatever % 2 == 1:
+            print whatever
+            find_odds(input)
