cee8d4d904efc371fc00fa1ed9f2e3c0f52d8a1a
vianelys <vianelys.montero@ms223.org>
2016-04-11 08:43:39 -0400

    Create april11_guidedpractice.py

---
+++ april11_guidedpractice.py
@@ -0,0 +1,17 @@
+def print_only(x):
+   y = x * 2
+   print y
+
+# let's try to use our 2 functions
+print "running print_only ..."
+print_only(7)
+
+def print_only(x):
+   y = x * 2
+   print y
+
+# let's try to use our 2 functions
+print "running print_only ..."
+print_only(7)
+# result: running print_only...
+#14
