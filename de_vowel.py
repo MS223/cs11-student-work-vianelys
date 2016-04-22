27611ceeae0eb51d0dabdd58d95b4f303e7fd9f8
vianelys <vianelys.montero@ms223.org>
2016-04-22 08:43:33 -0400

    Create de_vowel.py

---
+++ de_vowel.py
@@ -0,0 +1,6 @@
+def de_vowel(some_string):
+    for n in some_string:
+        if n != 'a' and n != 'e' and n != 'i' and n != 'o' and n != 'u':
+            return n
+print de_vowel
+#this came up : <function de_vowel at 0x1004bc500>. Like what the eff is that?!
