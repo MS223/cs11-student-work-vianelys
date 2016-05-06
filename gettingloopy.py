73a9ea1f1359b3087bd12ea5047c18a76d53e167
vianelys <vianelys.montero@ms223.org>
2016-05-06 08:39:02 -0400

    Create gettingloopy.py

---
+++ gettingloopy.py
@@ -0,0 +1,17 @@
+print "hello"[1:]
+#it dropped the 'h'
+print "hello"[:4]
+#it dropped the 'o'
+print "hello"[2:4]
+#it only kept the 'll'
+
+print "github"[1:3]
+print "hamburger"[3:]
+print "dongle"[1:3]
+print "snapchat"[1:4]
+
+def fruit_pluralizer(list_of_strings):
+if fruit [len(fruit)-1]=='y'
+    print fruit[:len(fruit)-1]+'ies'
+else:
+    print fruit #make the ies not show up on fruits like banana
